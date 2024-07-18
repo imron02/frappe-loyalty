27ba0628b02d3cc4aa7e95ca967a834c1891954d
Imron Rosdiana <imron.rosdiana@mymindstores.com>
2024-07-18 16:35:08 +0700

    Add loyalty apps

---
+++ loyalty/loyalty/doctype/sub_prize/sub_prize_list.js
@@ -0,0 +1,20 @@
+frappe.listview_settings['Sub-Prize'] = {
+	// Hide name column is equal to hide ID column on list view
+  hide_name_column: true,
+
+	// get_indicator: function (doc) {
+  //   console.log('doc', doc);
+  //   var colors = {
+  //     1: 'blue',
+  //     0: 'gray'
+  //   }
+
+  //   var labels = {
+  //     1: 'Default',
+  //     0: 'Not Default'
+  //   }
+
+  //   return [__('Unknown'), 'red']
+  // }
+}
+
